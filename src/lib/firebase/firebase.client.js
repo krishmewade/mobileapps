112bbc6670f3aeb7a77ac8d33d163f6e8d898fce
Krish <krish.mewade21@gmail.com>
2023-05-17 00:41:41 -0500

    try 3

--- src/lib/firebase/firebase.client.js
+++ src/lib/firebase/firebase.client.js
@@ -5,15 +5,16 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth'
 
+
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyCmXpez9xiMX4XEvKzScn9gOkXGYKjI7ck",
-  authDomain: "finalproject-7ce8e.firebaseapp.com",
-  projectId: "finalproject-7ce8e",
-  storageBucket: "finalproject-7ce8e.appspot.com",
-  messagingSenderId: "702721177565",
-  appId: "1:702721177565:web:2040924270852b638c6834",
-  measurementId: "G-0GDSEVZ9T5"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
